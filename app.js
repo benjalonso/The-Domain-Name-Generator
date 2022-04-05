a5b1dcd9c9e28ff6ddd9c669a044013be4723805
benjalonso <alonsobenjaminlb@gmail.com>
2022-04-05 15:13:42 +0000

    avances del generador

---
+++ app.js
@@ -0,0 +1,15 @@
+const subject = [
+    "the", "a", "my", "our"
+];
+const verb = [
+    "lastOfUs",
+    "kindOfWeb",
+    "virtualCorner",
+    "space"
+];
+const domain = [
+    ".com",
+    ".net",
+    ".cl",
+    ".dev"
+];
